11029b2c80c160e5d045da80c187034255506621
lebrito88 <lebrito88@gmail.com>
2022-01-23 16:49:34 -0300

    Informações do dashboard do Cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,9 @@
 
 describe('home page',()=>{
     it('app deve estar online', ()=> {
-        cy.viewport(1440,900)
-        cy.visit('https://buger-eats.vercel.app')
+       // cy.viewport(1440,900)  - já definido no cypress.json
+       // cy.visit('https://buger-eats.vercel.app') cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
 
     })
